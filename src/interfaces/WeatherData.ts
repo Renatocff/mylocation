425fcfeb300c7b31ca63d1b70199b60f2af4d664
Renato <renatocff@gmail.com>
2022-07-21 20:50:32 -0300

    Removidas propriedades  não utitilizadas em interface

--- src/interfaces/WeatherData.ts
+++ src/interfaces/WeatherData.ts
@@ -2,20 +2,11 @@ export interface IwheatherData {
     name: string;
     weather: [
         {
-            id: number,
-            main: string,
             description: string,
-            icon: string,
         }
     ],
-    wind: {
-        speed: string,
-        deg: number,
-        gust: number
-    },
     sys: {
         country: string,
-        sunrise: string,
     },
     main: {
         temp: string,
